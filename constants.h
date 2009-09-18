b87e86528bc559aa03c7fc8fc1f3910819f9884d
Jason <Jason@b6adef14-b3e5-4cdc-8498-51352f9f18dc>
2009-09-18 20:11:23 +0000

    git-svn-id: http://svn.osdrawer.net/aesaddon@6 b6adef14-b3e5-4cdc-8498-51352f9f18dc

---
+++ constants.h
@@ -0,0 +1,22 @@
+/* constants*/
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+
+#define APP_SIGNATURE  "application/x-vnd.AESAddOn"
+
+const uint32 BTN_SAVE		= 'Btns';
+const uint32 BTN_CANCEL		= 'Btnc';
+
+// button
+const int32 KEY									='key ';
+
+const uint32 STRENGTH128						='S128';
+const uint32 STRENGTH192						='S192';
+const uint32 STRENGTH256						='S256';
+
+const uint32 ENCRYPT							='ECPT';
+const uint32 DECRYPT							='DCPT';
+
+
+#endif // CONSTANTS_H
